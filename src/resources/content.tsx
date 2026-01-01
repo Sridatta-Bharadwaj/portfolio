2d45f577be31370ad20d84f515903f0ac38bbbc9
Sridatta-Bharadwaj <sridatta.bharadwaj2006@gmail.com>
2026-01-01 19:06:56 +0530

    cli testing

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -2,8 +2,8 @@ import { Blog, Gallery, Home, Newsletter, Person, Social, Work, Certifications }
 import { Line, Row, Text } from "@once-ui-system/core";
 
 const person: Person = {
-  firstName: "Sridatta Bharadwaj",
-  lastName: "Parupudi",
+  firstName: "Sridatta",
+  lastName: "Bharadwaj Parupudi",
   name: `Sridatta Bharadwaj Parupudi`,
   role: "Student",
   avatar: "/images/gallery/avatar3.png",
